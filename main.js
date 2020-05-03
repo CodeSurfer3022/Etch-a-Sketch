a1fb14d98e3884884bc99005073cb60ad2f479ec
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-05-03 17:29:19 +0530

    Add pen functionality

--- main.js
+++ main.js
@@ -22,26 +22,17 @@ function changeColor(e) {
     red = parseInt(selectedColor.slice(1,3), 16);
     green = parseInt(selectedColor.slice(3,5), 16);
     blue = parseInt(selectedColor.slice(5,7), 16);
-    // console.log(red, green, blue);
+    console.log(red, green, blue);
 }
 
-function setSize(e) {
-    currentSize = size;
+function changeSize(e) {
     size= e.toElement.id;
-    console.log(currentSize);
     console.log(size);
-    redrawGrid(currentSize, size);
+    let oldGrid = document.querySelector('#grid');
+    container.removeChild(oldGrid);
+    drawGrid(size);
+    addEventListeners();
 }
-
-// function redrawGrid(currentSize, size) {
-//     console.log("redrawing grid")
-//     for (let i = 0; i < currentSize; i ++) {
-//         for (let j = 0; j < currentSize; j ++) {
-//             console.log(grid);
-//             grid.removeChild('div');
-//          }
-//     }
-// }
 
 function drawGrid(size) {
     const grid = document.createElement('div');
@@ -95,6 +86,7 @@ function colorDiv(e) {
     
 function resetGrid() {
     // Reset the screen area to default color
+    const divs = document.querySelectorAll('.divs');
     divs.forEach(div => div.style.backgroundColor = gridDefaultColor);
 
     // Reset all global values to default
@@ -108,40 +100,50 @@ function resetGrid() {
     green = 255;
     blue = 255;
 
+    const colorPicker = document.querySelector('#penColor');
     colorPicker.value = `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 }    
 
-// Add a container to main 
-// Add the grid to container
+function addEventListeners() {
+    const colorPicker = document.querySelector('#penColor');
+    console.log(colorPicker.value);
+    colorPicker.addEventListener('change', changeColor);
+
+    const pens = document.querySelectorAll('#pens img');
+    pens.forEach(pen => {
+        pen.addEventListener('click', changeSize)
+    });
+
+    const divs = document.querySelectorAll('.divs');
+    divs.forEach( div => div.addEventListener('mouseover', colorDiv) );
+
+    const reset = document.querySelector('#reset');
+    reset.addEventListener('click', resetGrid);
+
+    const ludicrous = document.querySelector('#ludicrous');
+    ludicrous.addEventListener('click', () => {mode = "ludicrous"});
+
+    const darken = document.querySelector('#darken');
+    darken.addEventListener('click', () => mode= "darken");
+
+    const lighten = document.querySelector('#lighten');
+    lighten.addEventListener('click', () => mode= "lighten");
+}
+///////////////////////////////////////////////////////////////////////////////////////////
+// Code begins here
+///////////////////////////////////////////////////////////////////////////////////////////
 const main = document.querySelector('main');
+
+// Add a container to main 
 const container = document.createElement('div');
 container.setAttribute('id', 'container');
+main.appendChild(container);
+
+// Draw the initial grid
 drawGrid(size);
-main.appendChild(container)
 
-const colorPicker = document.querySelector('#penColor');
-console.log(colorPicker.value);
-colorPicker.addEventListener('change', changeColor);
+addEventListeners();
 
-const pens = document.querySelectorAll('#pens img');
-pens.forEach(pen => {
-    pen.addEventListener('click', setSize)
-});
-
-const divs = document.querySelectorAll('.divs');
-divs.forEach( div => div.addEventListener('mouseover', colorDiv) );
-
-let reset = document.querySelector('#reset');
-reset.addEventListener('click', resetGrid);
-
-let ludicrous = document.querySelector('#ludicrous');
-ludicrous.addEventListener('click', () => {mode = "ludicrous"});
-
-let darken = document.querySelector('#darken');
-darken.addEventListener('click', () => mode= "darken");
-
-let lighten = document.querySelector('#lighten');
-lighten.addEventListener('click', () => mode= "lighten");
 
 
 
