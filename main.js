1e3f1f7bfb18168f0c9638d3f43a219da0505098
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-05-04 17:42:42 +0530

    Make color picker responsive in all modes

--- main.js
+++ main.js
@@ -20,11 +20,31 @@ let green = 34;
 let blue = 255;
 
 /////////////////////////////////////////////////////////////////////////////////////
+function rgbToHex(red, green, blue) {
+    r = red.toString(16);
+    g = green.toString(16);
+    b = blue.toString(16);
+
+    r = r.length == 1 ? "0" + r : r;
+    g = g.length == 1 ? "0" + g : g;
+    b = b.length == 1 ? "0" + b : b;
+
+    return '#' + r + g + b;
+}
+
+
 function changeColor(e) {
     const selectedColor = e.target.value;
     red = parseInt(selectedColor.slice(1,3), 16);
     green = parseInt(selectedColor.slice(3,5), 16);
     blue = parseInt(selectedColor.slice(5,7), 16);
+}
+
+function changeColorPicker(red, green, blue) {
+    hexColor = rgbToHex(red, green, blue);
+    const colorPicker = document.querySelector('#penColor');
+    console.log(hexColor);
+    colorPicker.value = hexColor;
 }
 
 function changeSize(e) {
@@ -36,13 +56,27 @@ function changeSize(e) {
     addEventListeners();
 }
 
+function changeMode(e) {
+
+    mode = e.toElement.id;
+    const btns = document.querySelectorAll('button');
+    btns.forEach(btn => {
+        if (btn.getAttribute('class') === 'selected-button') {
+            btn.removeAttribute('class');
+        }
+    });
+
+    const btn = document.querySelector(`#${mode}`);
+    btn.classList.add('selected-button');
+}
+
 function drawGrid(size) {
     const grid = document.createElement('div');
-    // grid.textContent = 'this is a grid';
     grid.style.cssText = `display: grid;
                       grid-template: repeat(${size}, 1fr) / repeat(${size}, 1fr);`;            
     grid.setAttribute('id', 'grid'); 
     console.log("drawing grid");
+
     // add a div for each grid(and assign it to each grid area too)
     for (let i = 0; i < size; i ++) {
         for (let j = 0; j < size; j ++) {
@@ -56,8 +90,8 @@ function drawGrid(size) {
 }
 
 function defaultSelections() {
-    // const colorPicker = document.querySelector('#penColor');
-    // colorPicker.value = `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+    const btn = document.querySelector(`#${mode}`);
+    btn.classList.add('selected-button');
 
     const pens = document.querySelectorAll('#pens img');
     pens.forEach(pen => {
@@ -74,10 +108,10 @@ function defaultSelections() {
     const info = document.querySelector(`#${key}`);
     const disableInfo = document.querySelector(`#${otherKey}`);
     
-    info.classList.add('selectedInfo');
+    info.classList.add('selected-info');
     const exists = disableInfo.getAttribute('class');
     if (exists)
-        disableInfo.classList.remove('selectedInfo');
+        disableInfo.classList.remove('selected-info');
 }
 
 
@@ -92,20 +126,26 @@ function colorDiv(e) {
             green = Math.floor(Math.random() * 256);
             blue = Math.floor(Math.random() * 256);
             e.target.style.backgroundColor = `rgb(${red},${green},${blue})`;
+            
+            changeColorPicker(red, green, blue);
             break;
         
         case "darken":
-            red --;
-            green --;
-            blue --;
+            red = red - 1 > 0 ? red - 1 : 0;
+            green = green - 1 > 0 ? green - 1 : 0;
+            blue = blue - 1 > 0 ? blue - 1 : 0;
             e.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+            
+            changeColorPicker(red, green, blue);
             break;
 
         case "lighten":
-            red ++;
-            green ++;
-            blue ++;
+            red = red + 1 < 255 ? red + 1 : 255;
+            green = green + 1 < 255 ? green + 1 : 255;
+            blue = blue + 1 < 255 ? blue + 1 : 255;
             e.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+            
+            changeColorPicker(red, green, blue);
             break;
     }
 }
@@ -121,10 +161,10 @@ function togglePainting(e) {
     const info = document.querySelector(`#${key}`);
     const disableInfo = document.querySelector(`#${otherKey}`);
     
-    info.classList.add('selectedInfo');
+    info.classList.add('selected-info');
     const exists = disableInfo.getAttribute('class');
     if (exists)
-        disableInfo.classList.remove('selectedInfo');
+        disableInfo.classList.remove('selected-info');
 
     if (key === 's') {
         paintMode = "selection";
@@ -141,7 +181,6 @@ function clearGrid() {
     const divs = document.querySelectorAll('.divs');
     divs.forEach(div => div.style.backgroundColor = gridDefaultColor);
 
-    mode = "normal";
     defaultSelections();
 }    
 
@@ -157,17 +196,21 @@ function addEventListeners() {
     const clear = document.querySelector('#clear-screen');
     clear.addEventListener('click', clearGrid);
 
+    const normal = document.querySelector('#normal');
+    normal.addEventListener('click', changeMode);
+
     const ludicrous = document.querySelector('#ludicrous');
-    ludicrous.addEventListener('click', () => {mode = "ludicrous"});
+    ludicrous.addEventListener('click', changeMode);
 
     const darken = document.querySelector('#darken');
-    darken.addEventListener('click', () => mode= "darken");
+    darken.addEventListener('click', changeMode);
 
     const lighten = document.querySelector('#lighten');
-    lighten.addEventListener('click', () => mode= "lighten");
+    lighten.addEventListener('click', changeMode);
 
     document.addEventListener('keydown', togglePainting);
 }
+
 ///////////////////////////////////////////////////////////////////////////////////////////
 // Code begins here
 ///////////////////////////////////////////////////////////////////////////////////////////
